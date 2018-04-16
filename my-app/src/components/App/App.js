2dd78d729e1c5b84ed5e4a3d21da2aee4217d4f0
hyojin.sarchet <hyojin5931@hanmail.net>
2018-04-16 09:47:16 -0400

    deployed on http://hyojin.surge.sh/

--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-// import { BrowserRouter, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "../Nav/Nav";
 import Home from "../Home/Home";
 import Project from "../Project/Project";
@@ -10,32 +10,23 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      // <BrowserRouter>
+      // <Router>
       <div className="app">
-        <Nav />
-        <h1 className="name">HYOJIN SARCHET</h1>
-        <h1 className="title">WEB DEVELOPER</h1>
-        <hr className="hr" />
+        {/* <Nav /> */}
+        <div className="hero">
+          <h1 className="name">HYOJIN SARCHET</h1>
+          <h1 className="title">WEB DEVELOPER</h1>
+          <hr className="hr" />
+        </div>
         <Home />
         <Project />
         <Contact />
         <Footer />
-
-        {/* <Link to="/" className="nav-home">
-            Home
-          </Link>
-          <Link to="/project" className="nav-project">
-            Projects
-          </Link>
-          <Link to="/contact" className="nav-contact">
-            Contact
-          </Link>
-
-          <Route exact path="/" component={Home} />
-          <Route path="/project" component={Project} />
-          <Route path="/contact" component={Contact} /> */}
+        <Route exact path="/" component={Home} />
+        <Route path="/project" component={Project} />
+        <Route path="/contact" component={Contact} /> */}
       </div>
-      // </BrowserRouter>
+      // </Router>
     );
   }
 }
